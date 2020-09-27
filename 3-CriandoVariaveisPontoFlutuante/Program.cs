cd899ede2bb0e3bcb8d7d41dac05814bc3cbab43
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-09-27 10:36:13 -0300

    Add project files.

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 - Criando variáveis com ponto flutuante");
+
+            double salario = 1200.70;
+            Console.WriteLine(salario);
+
+            double idade = 15.0 / 2;
+            Console.WriteLine(idade);
+
+            idade = 15 / 2;
+            Console.WriteLine("15 / 2 = " + idade);
+
+            idade = 15.0 / 2;
+            Console.WriteLine("15.0 / 2 = " + idade);
+
+            Console.WriteLine("Fim do programa. Tecle Enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 4 - Conversão e outros tipos numéricos");
+
+            double salario = 1200.50;
+
+            int salarioEmInteiro = (int)salario;
+
+            Console.WriteLine("Fim do programa. Tecle Enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
