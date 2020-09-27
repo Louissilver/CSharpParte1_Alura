a76107c88f9ad0ae58e8d4261973b7e550af261e
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-09-27 11:43:04 -0300

    Conclusão do projeto 5 e início/conclusão do projeto 6 sobre variaveis

--- 5-CaracteresETextos/Program.cs
+++ 5-CaracteresETextos/Program.cs
@@ -12,7 +12,23 @@ namespace _5_CaracteresETextos
         {
             Console.WriteLine("Executando projeto 5 - Textos e Caracteres");
 
+            // character
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
 
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia";
+            Console.WriteLine(titulo);
+            string cursosProgramacao = 
+@" - .NET 
+ - Java
+ - JavaScript";
+            Console.WriteLine(cursosProgramacao);
 
             Console.WriteLine("Fim da execução. Tecle enter para continuar...");
             Console.ReadLine();
---
+++ 6-AtribuicaoDeVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicaoDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 6 - Atribuição de Variáveis");
+
+            int idade = 21;
+            int idadeLuis = idade;
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeLuis);
+
+            Console.WriteLine("Fim da execução. Tecle enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
