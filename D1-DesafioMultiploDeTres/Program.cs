be759b5dfd403d3ec594b0aa3f1adb3e570d475c
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-09-27 22:49:57 -0300

    Laço for com break e desafios com laço for de multiplos de 3 e fatorial

---
+++ D1-DesafioMultiploDeTres/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D1_DesafioMultiploDeTres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Desafio Opcional: Múltiplos de 3");
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Fim da execução. Tecle enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ D2-Fatorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2_Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Desafio Opcional: Fatorial");
+            int fatorial = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write($"{i}! = ");
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j != 1)
+                    {
+                        Console.Write(" x ");
+                    }
+                    Console.Write(j);
+                }
+                fatorial *= i;
+                Console.WriteLine($" = {fatorial}");
+                Console.WriteLine();
+            }
+            Console.WriteLine("Fim da execução. Tecle enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
