c104ea510501c10b95064e1dd17cc1c96215442d
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-09-27 21:41:26 -0300

    Laço For

---
+++ CalculaPoupanca2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_CalculaPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 11 - Calcula Poupança com For");
+
+            double valorInvestido = 1000.0;
+
+            for (int i = 1; i <= 12; i++)
+            {
+                valorInvestido += valorInvestido * 0.0036;
+                if (i == 1)
+                {
+                    Console.WriteLine($"Após {i} mês, você terá R${valorInvestido} reais");
+                }
+                else
+                {
+                    Console.WriteLine($"Após {i} meses, você terá R${valorInvestido} reais");
+                }
+            }
+            
+            Console.WriteLine("Fim da execução. Tecle enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
