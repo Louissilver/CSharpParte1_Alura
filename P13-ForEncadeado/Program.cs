7268ecaf413f3c1dc90e103dc4e2f1f3757bd382
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2020-09-27 22:09:22 -0300

    Encadeamento de For usando break

---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 13 - For encadeado");
+            // Usando comando break
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write("*");
+                    if (j >= i)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+            //Forma diferente
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Fim da execução. Tecle enter para continuar...");
+            Console.ReadLine();
+        }
+    }
+}
